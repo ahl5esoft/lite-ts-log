48d6705b3ef5cd2a696973803b270571ea56f728
linhongjue <linhongjue@fengling.cn>
2023-04-06 14:08:39 +0800

    feat:初始化项目

---
+++ src/log.ts
@@ -0,0 +1,27 @@
+import { ILog } from './i-log';
+
+export class ConsoleLog implements ILog {
+    private m_Labels: { [key: string]: any; } = {};
+
+    public addLabel(k: string, v: any): this {
+        this.m_Labels[k] = v;
+        return this;
+    }
+
+    public debug() {
+        console.debug(this.m_Labels);
+    }
+
+    public error(err: Error) {
+        this.addLabel('err', err);
+        console.error(this.m_Labels);
+    }
+
+    public info() {
+        console.info(this.m_Labels);
+    }
+
+    public warning() {
+        console.warn(this.m_Labels);
+    }
+}
